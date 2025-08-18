cfee5b8410f4b96ca592458c04314af074b1856e
shivanshcoding <shivanshcoding2005@gmail.com>
2025-08-18 13:16:20 +0530

    Correction in Raab Game

--- Introductory Problems/RaabGame1.cpp
+++ Introductory Problems/RaabGame1.cpp
@@ -1,48 +1,52 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int T;
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, a, b;
-        cin >> n >> a >> b;
+int main(){
+    scanf("%d", &T);
+    for(int t = 0, n, a, b; t < T; t++){
+        scanf("%d %d %d\n", &n, &a, &b);
 
-        if (a + b > n) {
-            cout << "NO\n";
+        // Degenerate case: all ties
+        if(a == 0 && b == 0){
+            printf("YES\n");
+            for(int i = 0; i < 2; i++)
+                for(int j = 1; j <= n; j++)
+                    printf("%d%c", j, (" \n")[j==n]);
             continue;
         }
 
-        cout << "YES\n";
-        vector<int> A, B;
-
-        // Step 1: give A 'a' wins
-        for (int i = 0; i < a; i++) {
-            A.push_back(n - i);      // A gets large numbers
-            B.push_back(i + 1);      // B gets small numbers
+        // Otherwise, each player must win at least once
+        // and the total wins cannnot exceed n
+        if(a + b > n || a == 0 || b == 0){
+            printf("NO\n");
+            continue;
         }
 
-        // Step 2: give B 'b' wins
-        for (int i = 0; i < b; i++) {
-            A.push_back(a + i + 1);  // A gets next available small
-            B.push_back(n - i);      // B gets large numbers
-        }
+        // General case: trade wins then handle ties
+        printf("YES\n");
+        int ties = n - a - b;
+        vector<int> xs(n+1);
 
-        // Step 3: remaining rounds are ties
-        int used = a + b;
-        for (int i = used + 1; i <= n; i++) {
-            A.push_back(i);
-            B.push_back(i);
+        // Start with both players playing 1, 2, 3, ...
+        // Cyclic shift a player by k to the right
+        // in order to give them k wins
+        // E.g. n = 5, shift b by k = 2
+        // as = 1 2 3 4 5 (3 wins)
+        // bs = 4 5 1 2 3 (2 wins)
+        for(int i = 1; i <= a+b; i++){
+            int pos = (i + b - 1) % (a + b) + 1;
+            xs[pos] = i;
         }
+        for(int i = a+b+1; i <= n; i++)
+            xs[i] = i;
 
-        // print result
-        for (int x : A) cout << x << " ";
-        cout << "\n";
-        for (int x : B) cout << x << " ";
-        cout << "\n";
+        // Print out solution
+        for(int i = 1; i <= n; i++)
+            printf("%d%c", i, (" \n")[i==n]);
+        for(int i = 1; i <= n; i++)
+            printf("%d%c", xs[i], (" \n")[i==n]);
     }
-    return 0;
 }
