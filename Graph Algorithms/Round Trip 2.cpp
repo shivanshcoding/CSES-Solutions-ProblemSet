89aa90ccbe8eaa477e7a7c381975dc63e313d5f1
shivanshcoding <shivanshcoding2005@gmail.com>
2026-03-06 13:01:36 +0530

    Grind for 6 March 2026

--- Graph Algorithms/Round Trip 2.cpp
+++ Graph Algorithms/Round Trip 2.cpp
@@ -1,53 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct custom_hash {
-   static uint64_t splitmix64(uint64_t x) {
-       x += 0x9e3779b97f4a7c15;
-       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-       x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-       return x ^ (x >> 31);
-   }
-size_t operator()(uint64_t x) const {
-       static const uint64_t FIXED_RANDOM =
-       chrono::steady_clock::now().time_since_epoch().count();
-       return splitmix64(x + FIXED_RANDOM);
-   }
-};
-#define prDouble(x) cout<<fixed<<setprecision(10)<<x
-#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define pb push_back
-#define f first
-#define s second
-#define sz(x) (int)(x).size()
-using ll = long long;
-using ld = long double;
-using pll = pair<ll,ll>;
-using tll = tuple<ll,ll,ll>;
-using vll = vector<ll>;
-using vpll = vector<pll>;
-vector<ll> dx = {1, -1, 0, 0}, dy = {0, 0, 1, -1}; // for grid
-vector<ll> ddx = {1,1,0,-1,-1,-1,0,1}, ddy = {0,1,1,1,0,-1,-1,-1}; // 8 directions
-template<typename T> void read(vector<T> &v) { for (auto &x : v) cin >> x; }
-template<typename T> void printv(const vector<T>& v) { for (auto &x : v) cout << x << ' '; }
-template<typename T> void print2d(const vector<vector<T>>& v) { for (auto &row : v) { for (auto &x : row) cout << x << ' '; cout << '\n'; } }
-ll t=1,n,m,p,q,r,k,a,b,c,x,y,z;
-const ll INF = 1e18, MOD = 1e9+7;
 
-void solve() {
-    cin>>n>>m;
-    vector<vector<ll>> adj(n);
-    for(int i=0;i<m;i++){
-        cin>>a>>b;
-        a--;b--;
-        adj[a].push_back(b);
+const int N = 1e5 + 5;
+vector<int> adj[N];
+bool vis[N], inStack[N];
+int parent[N];
+int n, m;
+int startNode = -1, endNode = -1;
+
+bool dfs(int u) {
+    vis[u] = true;
+    inStack[u] = true;
+
+    for (int v : adj[u]) {
+        if (!vis[v]) {
+            parent[v] = u;
+            if (dfs(v)) return true;
+        }
+        else if (inStack[v]) {
+            startNode = v;
+            endNode = u;
+            return true;
+        }
     }
+
+    inStack[u] = false;
+    return false;
 }
 
 int main() {
-    fastio();
-    // cin >> t;
-    while (t--) solve();
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            if (dfs(i)) break;
+        }
+    }
+
+    if (startNode == -1) {
+        cout << "IMPOSSIBLE\n";
+        return 0;
+    }
+
+    vector<int> cycle;
+    cycle.push_back(startNode);
+
+    for (int v = endNode; v != startNode; v = parent[v])
+        cycle.push_back(v);
+
+    cycle.push_back(startNode);
+    reverse(cycle.begin(), cycle.end());
+
+    cout << cycle.size() << "\n";
+    for (int x : cycle) cout << x << " ";
 }
